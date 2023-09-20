8a24818778ab69794358449e41a3e5f3649da77c
Galina Matvienko <galyagmgm@gmail.com>
2023-09-20 09:39:47 +0200

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
-app.use('/', express.static(join(__dirname, 'public')))
+// app.use('/', express.static(join(__dirname, 'public')))
 app.use("/", router());
 
 
